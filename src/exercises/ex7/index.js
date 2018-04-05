bd8ecb111cb8b77d608865894a2e7caa4fa0d73e
yoanribeiro <yoanribeiro@gmail.com>
2018-04-05 10:37:57 +0200

    Added ex7

---
+++ src/exercises/ex7/index.js
@@ -0,0 +1,30 @@
+function isEven(n){
+    return n % 2 === 0;
+}
+
+function evens(arr){
+    const res = [];
+    
+    for(let i = 0; i< arr.length; i++){
+        if(isEven(arr[i]))
+            res.push(arr[i]);
+    }
+
+    return res;
+}
+
+function odds(arr){
+    const res = [];
+
+    for(let i = 0; i< arr.length; i++){
+        if(!isEven(arr[i]))
+            res.push(arr[i]);
+    }
+
+    return res;
+}
+
+module.exports = {
+	evens,
+	odds,
+};
